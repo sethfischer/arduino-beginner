876429332e069747ff4fdd5968d98e9b0812f64b
Seth Fischer <seth@fischer.nz>
2021-06-14 17:14:04 +1200

    build: use isort profile black

--- lint-python.sh
+++ lint-python.sh
@@ -6,4 +6,4 @@ directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 git ls-files -z -- "${directory}/**.py" | xargs --no-run-if-empty --null flake8
 git ls-files -z -- "${directory}/**.py" | xargs --no-run-if-empty --null black --check
-git ls-files -z -- "${directory}/**.py" | xargs --no-run-if-empty --null isort --check-only
+git ls-files -z -- "${directory}/**.py" | xargs --no-run-if-empty --null isort --profile black --check-only
